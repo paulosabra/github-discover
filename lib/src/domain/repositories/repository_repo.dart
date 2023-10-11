a8cb35161c6fdfcde4d6a638f8506cb137c18acd
kauanNas <kauan802@gmail.com>
2023-10-11 20:17:53 -0300

    checkpoint flutter completo

---
+++ lib/src/domain/repositories/repository_repo.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:github_discover/src/data/utils/failure.dart';
+import 'package:github_discover/src/domain/entities/repository.dart';
+
+abstract class RepositoryRepo {
+  Future<Either<Failure, List<Repository>>> getRepositories({String? search});
+  Future<Either<Failure, Repository>> getRepository(String fullname);
+}
