28b9f282e45937cc51249c2b818c1d16a1372cbd
bruno.ferraz <bhferrazsilva@gmail.com>
2023-10-11 17:21:43 -0300

    fix: ajuste na implementação do repository

--- lib/src/data/repositories/repository_repository_impl.dart
+++ lib/src/data/repositories/repository_repository_impl.dart
@@ -1,19 +1,21 @@
 import 'dart:io';
 
 import 'package:dartz/dartz.dart';
-import 'package:github_discover/src/data/datasources/local/profile_local_data_source.dart';
-import 'package:github_discover/src/data/datasources/remote/profile_remote_data_source.dart';
-import 'package:github_discover/src/data/models/skill_model.dart';
+import 'package:github_discover/src/data/datasources/local/repository_local_data_source.dart';
+import 'package:github_discover/src/data/datasources/remote/repository_remote_data_source.dart';
 import 'package:github_discover/src/data/utils/exception.dart';
 import 'package:github_discover/src/data/utils/failure.dart';
-import 'package:github_discover/src/domain/entities/profile.dart';
 import 'package:github_discover/src/domain/entities/repository.dart';
-import 'package:github_discover/src/domain/entities/skill.dart';
-import 'package:github_discover/src/domain/repositories/profile_repository.dart';
 import 'package:github_discover/src/domain/repositories/repository_repository.dart';
 
 class RepositoryRepositoryImpl implements RepositoryRepository {
-  
+  final RepositoryLocalDataSource localDataSource;
+  final RepositoryRemoteDataSource remoteDataSource;
+
+  RepositoryRepositoryImpl({
+    required this.localDataSource,
+    required this.remoteDataSource,
+  });
 
   // @override
   // Future<Either<Failure, Profile>> getProfile() async {
@@ -52,12 +54,20 @@ class RepositoryRepositoryImpl implements RepositoryRepository {
   // }
 
   @override
-  Future<Repositories> getRepositoriesList() {
-    throw UnimplementedError();
+  Future<Repositories> getRepositoriesList() async {
+    final result = await localDataSource.getRepositoriesList();
+    return result.map((item) => item.toEntity()).toList();
   }
 
   @override
-  Future<Either<Failure, Repository>> getRepository() {
-    throw UnimplementedError();
+  Future<Either<Failure, Repository>> getRepository() async {
+    try {
+      final result = await remoteDataSource.getRepository();
+      return Right(result.toEntity());
+    } on ServerException {
+      return const Left(ServerFailure('Server Exception'));
+    } on SocketException {
+      return const Left(ConnectionFailure('Falha na conexão com a rede'));
+    }
   }
 }
