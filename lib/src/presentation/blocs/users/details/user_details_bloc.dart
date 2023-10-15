9eb57630dcddc75e08c9ac44ce6178742264fa7a
Tardelli46 <felipetardelli05@gmail.com>
2023-10-15 18:51:18 -0300

    cp2

--- lib/src/presentation/blocs/users/details/user_details_bloc.dart
+++ lib/src/presentation/blocs/users/details/user_details_bloc.dart
@@ -1,31 +1,39 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:github_discover/src/constants/mock/repository_mock.dart';
-import 'package:github_discover/src/constants/mock/user_mock.dart';
 import 'package:github_discover/src/domain/entities/repository.dart';
 import 'package:github_discover/src/domain/entities/user.dart';
+import 'package:github_discover/src/domain/usecases/users/get_user_usecase.dart';
+
 
 part 'user_details_event.dart';
 part 'user_details_state.dart';
 
 class UserDetailsBloc extends Bloc<UserDetailsEvent, UserDetailsState> {
-  UserDetailsBloc() : super(UserDetailsInitial()) {
+  final GetUserUseCase getUserUseCase;
+
+  UserDetailsBloc({required this.getUserUseCase})
+      : super(UserDetailsInitial()) {
     on<UserDetailLoadedEvent>(_onUserDetailLoadedEvent);
   }
+
 
   void _onUserDetailLoadedEvent(
     UserDetailLoadedEvent event,
     Emitter emit,
   ) async {
-    emit(const UserDetailsSuccess(
-      user: kUserMock,
-      repositories: [
-        kRepositoryMock,
-        kRepositoryMock,
-        kRepositoryMock,
-        kRepositoryMock,
-        kRepositoryMock,
-      ],
-    ));
+    emit(UserDetailsLoading());
+
+    final userResult = await getUserUseCase.execute(event.fullName!);
+    userResult.fold(
+      (failure) {
+        emit(UserDetailsError(
+          message: failure.message,
+        ));
+      },
+      (data) {
+        emit(UserDetailsSuccess(user: data, repositories: [])
+        );
+      },
+    );
   }
 }
